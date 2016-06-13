47e727e7cb7d2eb21b69bb5d5338be8b081ec11c
Jinal <Jinal@JSN>
2016-06-13 18:54:45 -0400

    final

---
+++ Lab2/Students.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="Lab2.Students" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Lab2/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: Jinal Patel
+ * @date: june 13, 2016
+ * @version: 0.0.2 - updated SetActivePage method
+ */
+
+namespace Lab2
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        /**
+         * This method adds a css class of "active" to list items related
+         * to navigation links of each page 
+         * 
+         * @method SetActivePage
+         * @return {void}
+         */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Students":
+                    students.Attributes.Add("class", "active");
+                    break;
+                case "Courses":
+                    courses.Attributes.Add("class", "active");
+                    break;
+                case "Departments":
+                    departments.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+
+    }
+}
